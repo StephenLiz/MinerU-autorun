b9808843de539e598a896060b57dbefe66307f3c
kstephenlgz91@gmail.com <a13536651712a>
2025-01-20 22:32:42 +0800

    print Dockerfile add vim monitor_script.py move file and print success move file to cache

--- docker/china/monitor_script.py
+++ docker/china/monitor_script.py
@@ -3,18 +3,11 @@ import time
 import subprocess
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
-import shutil
 
 # 监视目录
 WATCH_DIR = "/input"
 # 输出目录
 OUTPUT_DIR = "/output"
-# 缓存目录
-CACHE_DIR = "/cache"
-
-# 确保缓存目录存在
-if not os.path.exists(CACHE_DIR):
-    os.makedirs(CACHE_DIR)
 
 # 激活虚拟环境
 venv_activate = "/opt/mineru_venv/bin/activate"
@@ -28,22 +21,8 @@ print(f"Magic-pdf path: {subprocess.check_output(['which', 'magic-pdf']).decode(
 print(f"inotifywait path: {subprocess.check_output(['which', 'inotifywait']).decode().strip()}")
 
 class PDFHandler(FileSystemEventHandler):
-    def __init__(self):
-        super().__init__()
-        print("PDFHandler initialized and ready to monitor files")
-
     def on_closed(self, event):
-        print("运行on_closed函数")
-        if not event.is_directory and event.src_path.endswith('.pdf'):
-            file_path = event.src_path
-            # 获取文件列表并按创建时间排序
-            files = sorted([f for f in os.listdir(WATCH_DIR) if f.endswith('.pdf')], key=lambda x: os.path.getctime(os.path.join(WATCH_DIR, x)))
-            for file_name in files:
-                src_file = os.path.join(WATCH_DIR, file_name)
-                dest_file = os.path.join(CACHE_DIR, file_name)
-                # 移动文件
-                shutil.move(src_file, dest_file)
-                print(f"{file_name} 已从 {WATCH_DIR} 移动到 {CACHE_DIR}")
+        print("Hello world")
 
 if __name__ == "__main__":
     event_handler = PDFHandler()
